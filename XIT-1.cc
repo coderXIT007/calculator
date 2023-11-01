b3d8ff31eb90eabb575d1fd8d405a21a747a5b02
Mr.X <149552286+coderXIT007@users.noreply.github.com>
2023-11-01 08:49:16 +0530

    Create XIT-1.cc
    Programmed in c++

---
+++ XIT-1.cc
@@ -0,0 +1,102 @@
+#include<iostream.h>
+ int main()
+ {
+  {
+  	cout<<" [Addition] "<<endl;
+  	cout<<" "<<endl;
+    int i,j,b;
+    cout<<"|INPUT FIRST NUMBER|"<<endl;
+    cin>>i;
+    cout<<"|INPUT SECOND NUMBER|"<<endl;
+    cin>>j;
+    cout<<"|RESULT|"<<endl;
+    b=i+j;
+    cout<<b<<endl;
+  }
+  cout<<" "<<endl;
+  cout<<" [Subtraction] "<<endl;
+  cout<<" "<<endl;
+    {
+ 	  int i,v,f;
+ 	  cout<<"|INPUT FIRST NUMBER|"<<endl;
+ 	  cin>>i;
+ 	  cout<<"|INPUT SECOND NUMBER|"<<endl;
+ 	  cin>>v;
+ 	  cout<<"|RESULT|"<<endl;
+ 	  f=v-i;
+ 	  cout<<f<<endl;
+ 	  {
+ 	  	cout<<" "<<endl;
+ 	  	cout<<" [Multiplication] "<<endl;
+ 	  	cout<<" "<<endl;
+ 	  	int n,s,d;
+  	 	cout<<"|INPUT FIRST NUMBER|"<<endl;
+  	 	cin>>n;
+  	 	cout<<"|INPUT SECOND NUMBER|"<<endl;
+  	 	cin>>s;
+  	 	cout<<"|RESULT|"<<endl;
+  	 	d=n*s;
+  	 	cout<<d<<endl;
+  	 	{
+		cout<<" "<<endl;
+		cout<<" [Division] "<<endl;
+		cout<<" "<<endl;
+        int g,h,x;
+	 	cout<<"|INPUT FIRST NUMBER|"<<endl;
+		cin>>g;
+		cout<<"|INPUT SECOND NUMBER|"<<endl;
+		cin>>h;
+		cout<<"|Result|"<<endl;
+		x=g/h;
+	 	cout<<x<<endl; 
+	 	cout<<" "<<endl;
+	 	cout<<" |Area and Circumference Finder|"<<endl;
+	 	cout<<" "<<endl;
+	   {
+        double r,cr,ar;
+        cout<<"|INPUT THE RADIUS|"<<endl;
+        cin>>r; 
+        ar=3.14*r*r;
+        cr=2*3.14*r;
+        cout<<"|RESULT|"<<endl;
+        cout<<"|Area|="<<ar<<"sq.cm"<<endl;
+        cout<<"|Circumference|="<<cr<<" "<<"cm"<<endl;
+        cout<<" "<<endl;
+        cout<<" [confirmation of your knowledge] "<<endl;
+        cout<<" "<<endl;
+    {
+    	cout<<"|Input Your Age|"<<endl;
+	  int a;
+	 cin>>a;
+	 cout<<"|Result|"<<endl;
+	 if(a>=7)
+	 {
+    		cout<<"|You now how to code with c++|";
+         }
+     else		
+         {
+    		cout<<"|You know little bit of coding|";
+         }
+         cout<<" "<<endl;
+   		   {
+   		   	cout<<" "<<endl;
+	    	cout<<"|Finish|"<<endl;
+		    cout<<" "<<endl;
+            cout<<"|Thanks for using my program|"<<endl;
+	        cout<<" "<<endl;
+		    cout<<"|See you later|"<<endl;
+		    cout<<" "<<endl;
+	     }
+	     cout<<" "<<endl;
+        }
+        cout<<" "<<endl;
+       }
+       cout<<"________________X________________"<<endl;
+       cout<<" "<<endl;
+ 	  }
+ 	  cout<<" "<<endl;
+  	 }
+  	 cout<<" "<<endl;
+    }
+    cout<<" "<<endl;
+ }
